979d3fab991d644bc1f0ee932b49a059d43f35b7
Serhii Pylypenko <serhii.pylypenko.0@gmail.com>
2023-11-25 14:16:34 +0200

    Errors fixed
    1. Implemented method 'handleRequest' of class UpdateUserFunction;
    2. Implemented method 'updateUser' of class DynamoDBServiceImpl.

--- DynamoDBApp/src/main/java/handler/UpdateUserFunction.java
+++ DynamoDBApp/src/main/java/handler/UpdateUserFunction.java
@@ -9,20 +9,18 @@ import layer.service.APIGatewayServiceImpl;
 import layer.service.DynamoDBService;
 import layer.service.DynamoDBServiceImpl;
 
-
 public class UpdateUserFunction implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
-    private static final DynamoDBService dynamoDBService = new DynamoDBServiceImpl();
-    private static final APIGatewayService apiGatewayService = new APIGatewayServiceImpl();
+	private static final DynamoDBService dynamoDBService = new DynamoDBServiceImpl();
+	private static final APIGatewayService apiGatewayService = new APIGatewayServiceImpl();
 
-    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input,
-                                                      final Context context) {
-        //TODO Implement a record update with help of DynamoDBService
-        /*
-        Add code that extracts parameters and body from input,
-        and uses DynamoDBService to update the record in the table.
-        The result of operation provided as HTTP 200 response.
-        In case of error return HTTP 503.
-        */
-    }
+	public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
+		try {
+			String output = dynamoDBService.updateUser(input.getPathParameters(), input.getBody());
+			return apiGatewayService.getApiGatewayProxyResponseEvent(output, 200);
+		} catch (Exception e) {
+			return apiGatewayService
+					.getApiGatewayProxyResponseEvent("An error occurred while executing the lambda function: "
+							+ e.getClass() + "; message: " + e.getMessage(), 503);
+		}
+	}
 }
-
